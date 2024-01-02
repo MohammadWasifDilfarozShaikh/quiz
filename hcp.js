0dcb91c1d005924af8f2192eddf4968858df784c
shaikhwasif720@gmail.com <shaikhwasif720@gmail.com>
2024-01-02 22:17:11 +0530

    initial commit

---
+++ hcp.js
@@ -0,0 +1,97 @@
+let a = 0
+console.log(a)
+function result1()
+{
+    if(document.getElementById("inp1").value == 4)
+     {
+        const image = document.getElementById("img1")
+        image.style.display='block'
+        document.getElementById("score").innerHTML = a += 5
+        console.log("Right")
+        console.log(a)
+     }
+     else{
+        const image1 = document.getElementById("img2")
+        image1.style.display='block'
+        document.getElementById("score").innerHTML= a
+        console.log("wrong")
+    }
+}
+function result2()
+{
+
+    if(document.getElementById("inp2").value == 1)
+     {
+        const image = document.getElementById("img3")
+        image.style.display='block'
+        document.getElementById("score").innerHTML = a += 5
+        console.log("Right")
+        console.log(a)
+     }
+     else{
+        const image = document.getElementById("img4")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a
+        console.log("wrong")
+    }
+}
+function result3()
+{
+
+    if(document.getElementById("inp3").value == 1)
+     {
+        const image = document.getElementById("img5")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a += 5
+        console.log("Right")
+        console.log(a)
+     }
+     else{
+        const image = document.getElementById("img6")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a
+        console.log("wrong")
+    }
+}
+function result4()
+{
+
+    if(document.getElementById("inp4").value == 2)
+     {
+        const image = document.getElementById("img7")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a += 5
+        console.log("Right")
+        console.log(a)
+    }
+    else{
+        const image = document.getElementById("img8")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a
+        console.log("wrong")
+    }
+}
+function result5()
+{
+
+    if(document.getElementById("inp5").value == 1)
+     {
+        const image = document.getElementById("img9")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a += 5
+        console.log("Right")
+        console.log(a)
+    }
+    else{
+        const image = document.getElementById("img10")
+        image.style.display='block'
+        document.getElementById("score").innerHTML= a
+        console.log("wrong")
+    }
+}
+
+// function picture1()
+// {
+//     var uk = document.getElementById("inp1");
+//     uk.src="istockphoto-1079725292-612x612.jpg"
+// }
